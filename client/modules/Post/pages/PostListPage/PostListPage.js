3e7e61cca97e5691b71b5965c8cadc070ad995d9
Konrad Jakubowski <jakubowski.info@gmail.com>
2018-04-27 22:25:35 +0200

    Add simple fix

--- client/modules/Post/pages/PostListPage/PostListPage.js
+++ client/modules/Post/pages/PostListPage/PostListPage.js
@@ -19,7 +19,7 @@ class PostListPage extends Component {
   }
 
   handleDeletePost = post => {
-    if (confirm('Do you want to delete this post')) { // eslint-disable-line
+    if (confirm('Do you want to delete this post')) {
       this.props.dispatch(deletePostRequest(post));
     }
   };
